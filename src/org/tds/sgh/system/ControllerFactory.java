e635f219ce135885c002521b7b7e7641fab26c3e
Matias Pacheco <mpacheco@everlyhealth.com>
2022-12-17 10:18:07 -0300

    init repo

---
+++ src/org/tds/sgh/system/ControllerFactory.java
@@ -0,0 +1,57 @@
+package org.tds.sgh.system;
+
+import org.tds.sgh.business.CadenaHotelera;
+import org.tds.sgh.infrastructure.NotImplementedException;
+
+
+public class ControllerFactory implements IControllerFactory
+{
+	// --------------------------------------------------------------------------------------------
+	
+	private CadenaHotelera cadenaHotelera;
+	
+	// --------------------------------------------------------------------------------------------
+	
+	public ControllerFactory(CadenaHotelera cadenaHotelera)
+	{
+		this.cadenaHotelera = cadenaHotelera;
+	}
+	
+	// --------------------------------------------------------------------------------------------
+	
+	@Override
+	public ICadenaController createCadenaController()
+	{
+		return new CadenaController(this.cadenaHotelera);
+		// Actualizar a:
+		// return new SRController(this.cadenaHotelera);
+	}
+	
+	@Override
+	public ICancelarReservaController createCancelarReservaController()
+	{
+		// TODO
+		throw new NotImplementedException();
+	}
+	
+	@Override
+	public IHacerReservaController createHacerReservaController()
+	{
+		// TODO
+		throw new NotImplementedException();
+	}
+	
+	@Override
+	public IModificarReservaController createModificarReservaController()
+	{
+		// TODO
+		throw new NotImplementedException();
+	}
+	
+	@Override
+	public ITomarReservaController createTomarReservaController()
+	{
+		// TODO
+		throw new NotImplementedException();
+	}
+}
---
+++ src/org/tds/sgh/system/SRController.java
@@ -0,0 +1,18 @@
+package org.tds.sgh.system;
+
+import org.tds.sgh.business.CadenaHotelera;
+import org.tds.sgh.dtos.ClienteDTO;
+import org.tds.sgh.infrastructure.Infrastructure;
+
+public class SRController implements IAltaClienteController  {
+	
+	private CadenaHotelera cadenaHotelera; 
+
+	@Override
+	public ClienteDTO registrarCliente(String rut, String nombre, String direccion, String telefono, String mail) {
+		// Infrastructure.getInstance().getSistemaMensajeria().enviarMail(direccion, telefono, mail);
+		// Infrastructure.getInstance().getCalendario().getHoy();
+		// Infrastructure.getInstance().getCalendario().esAnterior(null, null)
+	}
+	
+}
